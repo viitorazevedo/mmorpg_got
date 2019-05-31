9c76150c8fe145e4970af03cdd7046f5a8acbbb6
viitorazevedo <42189057+viitorazevedo@users.noreply.github.com>
2019-05-31 20:00:17 -0300

    Aula 86
    Projeto evoluido até a aula 86.

---
+++ app/controllers/cadastro.js
@@ -0,0 +1,23 @@
+module.exports.cadastro = function(application, req, res){
+        res.render('cadastro', { validacao: {}, dadosForm: {}});
+}
+
+module.exports.cadastrar = function(application, req, res){
+        
+        var dadosForm = req.body;
+
+        req.assert('nome', 'Nome não pode ser vazio').notEmpty();
+        req.assert('usuario', 'Usuario não pode ser vazio').notEmpty();
+        req.assert('senha', 'Senha não pode ser vazio').notEmpty();
+        req.assert('casa', 'Casa não pode ser vazio').notEmpty();
+
+        var erros = req.validationErrors();
+
+        if(erros){
+                res.render('cadastro', {validacao: erros, dadosForm: dadosForm});
+                return;
+        };
+
+        res.send('Podemos cadastrar');
+};
+
