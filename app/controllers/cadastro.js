bd461ef4af6c1532f104d6f505b3f68738dceb37
viitorazevedo <42189057+viitorazevedo@users.noreply.github.com>
2019-06-03 17:34:47 -0300

    Aula 93 - controller de autenticação

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -18,6 +18,11 @@ module.exports.cadastrar = function(application, req, res){
                 return;
         };
 
+        var connection = application.config.dbConnection;
+        var UsuariosDAO = new application.app.models.UsuariosDAO(connection);
+
+        UsuariosDAO.inserirUsuario(dadosForm);
+
         res.send('Podemos cadastrar');
-};
+}
 
