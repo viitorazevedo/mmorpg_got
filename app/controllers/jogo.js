29cfcd8ae880f51436e9d0dccb1fbc89a9ffdab5
viitorazevedo <42189057+viitorazevedo@users.noreply.github.com>
2019-06-19 00:23:10 -0300

    Aula 99
    Ajustando bandeira casa do jogador

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,3 +1,15 @@
 module.exports.jogo = function(application, req, res){
+
+    if(req.session.autorizado){
     res.render('jogo');
+} else {
+    res.send('Usuário precisa fazer o login');
+}
+}
+
+module.exports.sair = function(application, req, res){
+
+ req.session.destroy( function(err){
+     res.render("index", { validacao: {} })
+ });
 }
