543434a6beb233b3b4f3caa2265cdc003c796c23
viitorazevedo <42189057+viitorazevedo@users.noreply.github.com>
2019-07-17 16:55:51 -0300

    Aula 113
    Finalização do projeto e alguns desafios

--- app/models/JogoDAO.js
+++ app/models/JogoDAO.js
@@ -1,3 +1,5 @@
+var ObjectID = require('mongodb').ObjectID;
+
 function JogoDAO(connection){
     this._connection = connection();
 }
@@ -50,8 +52,25 @@ JogoDAO.prototype.acao = function(acao){
             }
 
             acao.acao_termina_em = date.getTime() + tempo;
-            collection.insert(acao); 
-            mongoclient.close(acao);
+            collection.insert(acao);    
+        });
+
+        mongoclient.collection("jogo", function(err, collection){
+           
+            var moedas = null;
+
+            switch(parseInt(acao.acao)){
+                case 1: moedas = -2 * acao.quantidade; break;
+                case 2: moedas = -3 * acao.quantidade; break;
+                case 3: moedas = -1 * acao.quantidade; break;
+                case 4: moedas = -1 * acao.quantidade; break;
+            }
+            collection.update(
+                { usuario: acao.usuario},
+                { $inc: {moeda: moedas}}
+            );
+
+            mongoclient.close();
         });
     });
 }
@@ -71,6 +90,21 @@ JogoDAO.prototype.getAcoes = function(usuario, res){
           });
     });
 });
+}
+
+
+JogoDAO.prototype.revogarAcao = function(_id, res){
+    this._connection.open( function(err, mongoclient){
+        mongoclient.collection("acao", function(err, collection){
+            collection.remove(
+                {_id : ObjectID(_id)},
+                function(err, result){
+                    res.redirect("jogo?msg=D");
+                    mongoclient.close();  
+                }
+            );
+        });   
+    });
 }
 
 module.exports = function(){
